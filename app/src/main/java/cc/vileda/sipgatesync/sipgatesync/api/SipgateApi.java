db844a00a9018e76ded6f630d958f85d097162de
vileda <vileda@vileda.cc>
2016-04-08 18:57:34 +0200

    fix build

--- app/src/main/java/cc/vileda/sipgatesync/sipgatesync/api/SipgateApi.java
+++ app/src/main/java/cc/vileda/sipgatesync/sipgatesync/api/SipgateApi.java
@@ -9,7 +9,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
-import java.net.ProtocolException;
 import java.net.URL;
 
 /**
